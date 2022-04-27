d1f46bdcb494ccfab6191d3796735f368f70268b
Jan Heiland <jnhlnd@gmail.com>
2022-04-27 19:12:09 +0200

    bookdown like running

---
+++ mkdc.sh
@@ -0,0 +1,5 @@
+# R -e 'install.packages("kableExtra")'
+# R -e 'install.packages("bookdown")'
+
+Rscript -e 'bookdown::render_book("index.md", "bookdown::pdf_book", quiet=FALSE)'
+Rscript -e 'bookdown::render_book("index.md", "bookdown::gitbook", quiet=FALSE)'

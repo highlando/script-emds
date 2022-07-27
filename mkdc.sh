fd905174c25bb1662795f1c81f2816b59fd7a075
Jan Heiland <jnhlnd@gmail.com>
2022-07-27 09:47:32 +0200

    alles auskommentiert was nicht formel ist

--- mkdc.sh
+++ mkdc.sh
@@ -2,4 +2,6 @@
 # R -e 'install.packages("bookdown")'
 
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book", quiet=FALSE)'
-Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook", quiet=FALSE)'
+# Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook", quiet=FALSE)'
+
+# pdftk docs/EMDS.pdf cat 7 9 11 12 19 output formelsammlung.pdf
